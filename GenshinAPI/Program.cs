f929cc9299ed3e4793ef97e5cb728fc168209c94
Pierre-Bossis <pbossis@hotmail.com>
2024-01-04 15:20:27 +0100

    classe static injectiondependances

--- GenshinAPI/Program.cs
+++ GenshinAPI/Program.cs
@@ -2,6 +2,7 @@ using Genshin.BLL.Interfaces;
 using Genshin.BLL.Services;
 using Genshin.DAL.DataAccess;
 using Genshin.DAL.Repositories;
+using GenshinAPI.Services;
 using GenshinAPI.Tools;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
@@ -10,71 +11,13 @@ using System.Data.SqlClient;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-IConfiguration configuration = builder.Configuration;
+
+//Service lié aux injections de dépendances
+DependencyInjectionService.ConfigureDependencyInjection(builder.Services, builder.Configuration);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-#region Injections
-
-builder.Services.AddScoped<JwtGenerator>();
-builder.Services.AddTransient<DbConnection>(sp => new SqlConnection(configuration.GetConnectionString("techno")));
-builder.Services.AddScoped<IMateriauxElevationArmesRepository, MateriauxElevationArmesService>();
-builder.Services.AddScoped<IMateriauxElevationArmesBLLService, MateriauxElevationArmesBLLService>();
-
-builder.Services.AddScoped<IArmesRepository, ArmesService>();
-builder.Services.AddScoped<IArmesBLLService, ArmesBLLService>();
-
-builder.Services.AddScoped<IProduitsRepository,ProduitsService>();
-builder.Services.AddScoped<IProduitsBLLService,ProduitsBLLService>();
-
-builder.Services.AddScoped<IMateriauxAmeliorationPersonnagesRepository, MateriauxAmeliorationPersonnagesService>();
-builder.Services.AddScoped<IMateriauxAmeliorationPersonnagesBLLService,MateriauxAmeliorationPersonnagesBLLService>();
-
-builder.Services.AddScoped<IPersonnagesRepository, PersonnagesService>();
-builder.Services.AddScoped<IPersonnagesBLLService, PersonnagesBLLService>();
-
-builder.Services.AddScoped<IAvatarsRepository, AvatarsService>();
-builder.Services.AddScoped<IAvatarsBLLService, AvatarsBLLService>();
-
-builder.Services.AddScoped<IMateriauxElevationPersonnagesRepository, MateriauxElevationPersonnagesService>();
-builder.Services.AddScoped<IMateriauxElevationPersonnagesBLLService, MateriauxElevationPersonnagesBLLService>();
-
-builder.Services.AddScoped<ILivresAptitudeRepository, LivresAptitudeService>();
-builder.Services.AddScoped<ILivresAptitudeBLLService, LivresAptitudeBLLService>();
-
-builder.Services.AddScoped<IArmes_MateriauxElevationArmesRepository, Armes_MateriauxElevationArmesService>();
-builder.Services.AddScoped<IArmes_MateriauxElevationArmesBLLService, Armes_MateriauxElevationArmesBLLService>();
-
-builder.Services.AddScoped<IPersonnages_LivresAptitudeRepository, Personnages_LivresAptitudeService>();
-builder.Services.AddScoped<IPersonnages_LivresAptitudeBLLService, Personnages_LivresAptitudeBLLService>();
-
-builder.Services.AddScoped<IPersonnages_MateriauxElevationPersonnagesRepository, Personnages_MateriauxElevationPersonnagesService>();
-builder.Services.AddScoped<IPersonnages_MateriauxElevationPersonnagesBLLService, Personnages_MateriauxElevationPersonnagesBLLService>();
-
-builder.Services.AddScoped<IMateriauxAmeliorationPersonnagesEtArmesRepository, MateriauxAmeliorationPersonnagesEtArmesService>();
-builder.Services.AddScoped<IMateriauxAmeliorationPersonnagesEtArmesBLLService, MateriauxAmeliorationPersonnagesEtArmesBLLService>();
-
-builder.Services.AddScoped<IPersonnages_MatsAmelioPersosArmesRepository, Personnages_MatsAmelioPersosArmesService>();
-builder.Services.AddScoped<IPersonnages_MatsAmelioPersosArmesBLLService, Personnages_MatsAmelioPersosArmesBLLService>();
-
-builder.Services.AddScoped<IArmes_MatsAmelioPersosArmesRepository, Armes_MatsAmelioPersosArmesService>();
-builder.Services.AddScoped<IArmes_MatsAmelioPersosArmesBLLService, Armes_MatsAmelioPersosArmesBLLService>();
-
-builder.Services.AddScoped<IConstellationsRepository, ConstellationsService>();
-builder.Services.AddScoped<IConstellationsBLLService, ConstellationsBLLService>();
-
-builder.Services.AddScoped<IAptitudesRepository, AptitudesService>();
-builder.Services.AddScoped<IAptitudesBLLService, AptitudesBLLService>();
-
-builder.Services.AddScoped<IUserRepository,UserService>();
-builder.Services.AddScoped<IUserBLLService, UserBLLService>();
-
-builder.Services.AddScoped<IArtefactsRepository, ArtefactsService>();
-builder.Services.AddScoped<IArtefactsBLLService, ArtefactsBLLService>();
-
-#endregion
 
 #region Authentification
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
---
+++ GenshinAPI/Services/DependencyInjectionService.cs
@@ -0,0 +1,72 @@
+﻿using Genshin.BLL.Interfaces;
+using Genshin.BLL.Services;
+using Genshin.DAL.DataAccess;
+using Genshin.DAL.Repositories;
+using GenshinAPI.Tools;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace GenshinAPI.Services
+{
+    public static class DependencyInjectionService
+    {
+        public static void ConfigureDependencyInjection(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<JwtGenerator>();
+            services.AddTransient<DbConnection>(sp => new SqlConnection(configuration.GetConnectionString("techno")));
+            services.AddScoped<IMateriauxElevationArmesRepository, MateriauxElevationArmesService>();
+            services.AddScoped<IMateriauxElevationArmesBLLService, MateriauxElevationArmesBLLService>();
+
+            services.AddScoped<IArmesRepository, ArmesService>();
+            services.AddScoped<IArmesBLLService, ArmesBLLService>();
+
+            services.AddScoped<IProduitsRepository, ProduitsService>();
+            services.AddScoped<IProduitsBLLService, ProduitsBLLService>();
+
+            services.AddScoped<IMateriauxAmeliorationPersonnagesRepository, MateriauxAmeliorationPersonnagesService>();
+            services.AddScoped<IMateriauxAmeliorationPersonnagesBLLService, MateriauxAmeliorationPersonnagesBLLService>();
+
+            services.AddScoped<IPersonnagesRepository, PersonnagesService>();
+            services.AddScoped<IPersonnagesBLLService, PersonnagesBLLService>();
+
+            services.AddScoped<IAvatarsRepository, AvatarsService>();
+            services.AddScoped<IAvatarsBLLService, AvatarsBLLService>();
+
+            services.AddScoped<IMateriauxElevationPersonnagesRepository, MateriauxElevationPersonnagesService>();
+            services.AddScoped<IMateriauxElevationPersonnagesBLLService, MateriauxElevationPersonnagesBLLService>();
+
+            services.AddScoped<ILivresAptitudeRepository, LivresAptitudeService>();
+            services.AddScoped<ILivresAptitudeBLLService, LivresAptitudeBLLService>();
+
+            services.AddScoped<IArmes_MateriauxElevationArmesRepository, Armes_MateriauxElevationArmesService>();
+            services.AddScoped<IArmes_MateriauxElevationArmesBLLService, Armes_MateriauxElevationArmesBLLService>();
+
+            services.AddScoped<IPersonnages_LivresAptitudeRepository, Personnages_LivresAptitudeService>();
+            services.AddScoped<IPersonnages_LivresAptitudeBLLService, Personnages_LivresAptitudeBLLService>();
+
+            services.AddScoped<IPersonnages_MateriauxElevationPersonnagesRepository, Personnages_MateriauxElevationPersonnagesService>();
+            services.AddScoped<IPersonnages_MateriauxElevationPersonnagesBLLService, Personnages_MateriauxElevationPersonnagesBLLService>();
+
+            services.AddScoped<IMateriauxAmeliorationPersonnagesEtArmesRepository, MateriauxAmeliorationPersonnagesEtArmesService>();
+            services.AddScoped<IMateriauxAmeliorationPersonnagesEtArmesBLLService, MateriauxAmeliorationPersonnagesEtArmesBLLService>();
+
+            services.AddScoped<IPersonnages_MatsAmelioPersosArmesRepository, Personnages_MatsAmelioPersosArmesService>();
+            services.AddScoped<IPersonnages_MatsAmelioPersosArmesBLLService, Personnages_MatsAmelioPersosArmesBLLService>();
+
+            services.AddScoped<IArmes_MatsAmelioPersosArmesRepository, Armes_MatsAmelioPersosArmesService>();
+            services.AddScoped<IArmes_MatsAmelioPersosArmesBLLService, Armes_MatsAmelioPersosArmesBLLService>();
+
+            services.AddScoped<IConstellationsRepository, ConstellationsService>();
+            services.AddScoped<IConstellationsBLLService, ConstellationsBLLService>();
+
+            services.AddScoped<IAptitudesRepository, AptitudesService>();
+            services.AddScoped<IAptitudesBLLService, AptitudesBLLService>();
+
+            services.AddScoped<IUserRepository, UserService>();
+            services.AddScoped<IUserBLLService, UserBLLService>();
+
+            services.AddScoped<IArtefactsRepository, ArtefactsService>();
+            services.AddScoped<IArtefactsBLLService, ArtefactsBLLService>();
+        }
+    }
+}
